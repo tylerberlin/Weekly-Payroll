3ab6a7803f4a0548c8599f525fed8c58b6969412
tyler0611 <berlity0611@gmail.com>
2024-09-18 09:29:23 -0500

    Initial Commit

---
+++ Weekly Payroll/Weekly_PayrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weekly_PayrollApp.swift
+//  Weekly Payroll
+//
+//  Created by Tyler Berlin on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Weekly_PayrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
